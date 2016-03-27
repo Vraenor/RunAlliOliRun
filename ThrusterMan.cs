8241023d2e66dd82054b49dd9ec2979add4c7867
Vraenor <vicentwow26@gmail.com>
2016-03-27 17:02:30 +0200

    Scripts

---
+++ ThrusterMan.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrusterMan : MonoBehaviour {
+
+    public float thrusterStrength;
+    public float thrusterDistance;
+    public Transform[] thrusters;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+	void FixedUpdate () {
+
+        RaycastHit hit;
+
+        foreach (Transform thruster in thrusters)
+        {
+
+            Vector3 downwardForce;
+            float distancePercentage;
+
+            if (Physics.Raycast (thruster.position, thruster.up * -1, out hit, thrusterDistance))
+            {
+                // The thruster within thrusterDistance to the ground
+                distancePercentage = 1 - (hit.distance / thrusterDistance);
+
+                // Calculate how much force to push
+                downwardForce = transform.up * thrusterStrength * distancePercentage;
+
+                // Correct the force for the mass of the car and deltatime
+                downwardForce = downwardForce * Time.deltaTime * rigidbody.mass;
+
+                // Apply the force where the thruster is
+                rigidbody.AddForceAtPosition(downwardForce, thruster.position);
+            }
+
+        }
+	
+	}
+
+
+
+
+}
