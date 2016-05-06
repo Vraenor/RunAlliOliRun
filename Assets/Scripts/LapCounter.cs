e6478601dc1404dbc8a370b0e4baf91ce749a072
Froogygoogy <froogygoogy@gmail.com>
2016-05-06 18:00:50 +0200

    ui 50%

---
+++ Assets/Scripts/LapCounter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LapCounter : MonoBehaviour {
+
+
+    public GameObject player;
+    public NauMov playerMovement;
+    public Sprite[] numbers;
+    public Image lapCounter;                         // The image component of the lap.
+    public Image maxLap;                            // The image component of the lap.
+    public int lap;
+    public int maxLaps;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerMovement = player.GetComponent<NauMov>();
+        lap = 0;
+    }
+    void FixedUpdate()
+    {
+        switch (lap) {
+           
+            case 2:
+                lapCounter.sprite = numbers[1];
+                break;
+            case 3:
+                lapCounter.sprite = numbers[2];
+                break;
+
+            default:
+                lapCounter.sprite = numbers[0];
+                break;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            if (lap == 0) { lap = 1; }
+            if (lap == 1) { lap = 2; }
+            if (lap == 2) { lap = 3; }
+        }
+    }
+}
---
+++ Assets/Scripts/PosCounter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PosCounter : MonoBehaviour {
+
+    public GameObject player;
+    public NauMov playerMovement;
+    public Sprite[] numbers;
+    public Image posCounter;                         // The image component of the lap.
+    public int pos;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerMovement = player.GetComponent<NauMov>();
+        pos = 1;
+    }
+    void FixedUpdate()
+    {
+        switch (pos)
+        {
+            case 2:
+                posCounter.sprite = numbers[1];
+                break;
+            case 3:
+                posCounter.sprite = numbers[2];
+                break;
+
+            default:
+                posCounter.sprite = numbers[0];
+                break;
+        }
+
+    }
+
+}
